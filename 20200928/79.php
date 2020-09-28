d7300966a531d7b3e2b14dbc7a97cfe561749469
knagami <knagami008@yahoo.co.jp>
2020-09-28 14:56:01 +0900

    commit

--- 20200928/79.php
+++ 20200928/79.php
@@ -1,68 +1,27 @@
 <?php namespace Track;
 
 function main($lines) {
-  $list = array();
-
   foreach ($lines as $index=>$value) {
-    // printf("line[%s]: %s\n", $index, $value);
-    // $value = 都道府県,店舗名,売上
-    $values = explode(',', $value);
-
-    $prefectures = $values[0];
-    $store_name = $values[1];
-    $sales = $values[2];
-
-    // printf($prefectures);
-    // printf($store_name);
-    // printf($sales);
-
-    if (!array_key_exists($prefectures, $list)) {
-      // ない場合
-      $list += array(
-        $prefectures => array(
-          $store_name => $sales
-        )
-      );
-
-      // printf(0);
-    } else {
-      // ある場合
-      // 店舗を検索
-      if (!array_key_exists($store_name, $list[$prefectures])) {
-        // ない場合
-        $list[$prefectures] += array(
-          $store_name => $sales
-        );
-      } else {
-        // ある場合
-        $list[$prefectures][$store_name] += $sales;
+    $list=array();
+    $list=explode(",",$value);
+    $ALL[$list[0]][$list[1]]["sales"] += $list[2];
+  }
+  foreach ($ALL as $index1=>$value1){
+    $store_sum = 0;
+    printf("* ".$index1."\n");
+    foreach ($value1 as $index2=>$value2){
+      printf($index2." ");
+      foreach ($value2 as $index3=>$value3){
+        printf($value3."\n");
+        $store_sum += $value3;
       }
-      // printf(1);
     }
+    printf($index1."合計 ".$store_sum."\n");
+    $all_sum += $store_sum;
   }
-
-  // 結果表示
-  $country_total = 0; // 全国合計
-  foreach ($list as $key => $values) {
-    // 都道府県
-    printf("* %s\n", $key);
-
-    // 店舗
-    $total = 0; // 都道府県合計
-    foreach ($values as $name => $sales) {
-      printf("%s %s\n", $name, $sales);
-
-      $total += $sales;
-    }
-
-    // 都道府県合計
-    printf("%s合計 %s\n", $key, $total);
-
-    $country_total += $total;
-  }
-
-  // 全国合計
-  printf("全国合計 %s\n", $country_total);
+  printf("全国合計 ".$all_sum."\n");
+    
+    //printf("line[%s]: %s\n", $index, $value);
 }
 
 $array = array();
